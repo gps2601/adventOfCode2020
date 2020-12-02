05de848d1bc362e25cb42fa4733e2948d3970e36
gsmit592 <gsmit592@ford.com>
2020-12-02 07:18:07 +0000

    tidying up old job policy

--- src/main/kotlin/daytwo/PasswordValidator.kt
+++ src/main/kotlin/daytwo/PasswordValidator.kt
@@ -4,7 +4,6 @@ import daytwo.policies.OldJobPolicy
 import daytwo.policies.PasswordPolicy
 
 class PasswordValidator {
-
     fun isValid(passwordEntry: String, policy: PasswordPolicy = OldJobPolicy()): Boolean {
         return policy.isValid(passwordEntry)
     }
--- src/main/kotlin/daytwo/policies/OldJobPolicy.kt
+++ src/main/kotlin/daytwo/policies/OldJobPolicy.kt
@@ -2,14 +2,9 @@ package daytwo.policies
 
 class OldJobPolicy : PasswordPolicy {
     override fun isValid(passwordEntry: String): Boolean {
-        val entrySections = passwordEntry.split(" ")
-        val numberOfTimes = entrySections.first()
-        val min = numberOfTimes.split("-").first().toInt()
-        val max = numberOfTimes.split("-").last().toInt()
-        val pattern = entrySections[1].toCharArray().first()
-        val password = entrySections.last()
-
-        val occurrences = password.count{ char-> char == pattern}
-        return occurrences in min..max
+        val (rangePattern, passwordPattern, password) = passwordEntry.split(" ")
+        val range = rangePattern.split("-").map { it.toInt() }
+        val occurrences = password.count { char -> char == passwordPattern.removeSuffix(":").single() }
+        return occurrences in range.first()..range.last()
     }
 }
